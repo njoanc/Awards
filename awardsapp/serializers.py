c8fff283e4d95aeb698f9be558233c1c3a508925
melisamalala <melissamalala@gmail.com>
2018-10-10 22:03:23 +0300

    Added views functions for PROJECT SERIALIZERS

--- awardsapp/serializers.py
+++ awardsapp/serializers.py
@@ -1,1 +1,8 @@
 from rest_framework import serializers
+from .models import Project
+
+
+class ProjectSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Project
+        fields = ('title', 'project_image', 'description', 'project_url')
--- awardsapp/views.py
+++ awardsapp/views.py
@@ -5,8 +5,8 @@ from django.shortcuts import render
 
 # Create your views here.
 from django.shortcuts import render, redirect, get_object_or_404
-from .models import Image,Location,tags, Profile, Review, NewsLetterRecipients, Like
-from django.http  import HttpResponse, Http404, HttpResponseRedirect
+from .models import Image,Location,tags, Profile, Review, NewsLetterRecipients, Like, Project
+from django.http  import HttpResponse, Http404, HttpResponseRedirect, JsonResponse
 from django.contrib.auth.decorators import login_required
 from django.core.urlresolvers import reverse
 from django.contrib.auth.models import User
@@ -14,6 +14,11 @@ from .forms import NewImageForm, UpdatebioForm, ReviewForm
 from .email import send_welcome_email
 from .forms import NewsLetterForm
 from rest_framework import generics
+from rest_framework.response import Response
+from rest_framework.views import APIView
+from .serializers import ProjectSerializer
+from rest_framework import status
+from .permissions import IsAdminOrReadOnly
 
 # Views
 tags = tags.objects.all()
@@ -209,3 +214,54 @@ def individual_profile_page(request, username):
 
 
 # generic views
+
+class ProjectList(APIView):
+    def get(self, request, format = None):
+        all_projects = Project.objects.all()
+        serializers = ProjectSerializer(all_projects, many = True)
+        return Response(serializers.data)
+
+
+    def post(self, request, format=None):
+        serializers = ProjectSerializer(data=request.data)
+        permission_classes = (IsAdminOrReadOnly,)
+
+        if serializers.is_valid():
+            serializers.save()
+            return Response(serializers.data, status= status.HTTP_201_CREATED)
+
+        return Response(serializers.errors, status = status.HTTP_400_BAD_REQUEST)
+
+
+class ProjectDescription(APIView):
+    permission_classes = (IsAdminOrReadOnly,)
+    def get_project(self, pk):
+        try:
+            return Project.objects.get(pk=pk)
+        except Project.DoesNotExist:
+            return Http404
+
+    def get(self, request, pk, format=None):
+        project = self.get_project(pk)
+        serializers = ProjectSerializer(project)
+        return Response(serializers.data)
+
+
+    def put(self, request, pk, format = None):
+        project = self.get_project(pk)
+        serializers = ProjectSerializer(project, request.data)
+        if serializers.is_valid():
+            serializers.save()
+            return Response(serializers.data)
+
+        else:
+            return Response(serializers.errors,
+                            status = status.HTTP_400_BAD_REQUEST)
+
+    def delete(self, request, pk, format=None):
+        project = self.get_project(pk)
+        project.delete()
+        return Response(status=status.HTTP_204_NO_CONTENT)
+
+
+
